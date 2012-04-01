a8e21f2b307172c7b628f4a7e008e6b10b18cc9d
Shengyi Wang <txyyss@gmail.com>
2012-04-01 20:00:22 +0800

    Solve first 2 problems.

---
+++ Euler001.hs
@@ -0,0 +1,16 @@
+-- If we list all the natural numbers below 10 that are multiples of 3
+-- or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+-- Find the sum of all the multiples of 3 or 5 below 1000.
+
+module Euler where
+
+-- naive version
+result001 = sum [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
+
+-- more efficient version
+sumDivisibleBy :: Integral a => a -> a -> a
+sumDivisibleBy n upperLimit = n * p * (p+1) `div` 2
+  where p = upperLimit `div` n
+
+result001' = (sumDivisibleBy 3 999) + (sumDivisibleBy 5 999) - (sumDivisibleBy 15 999)
