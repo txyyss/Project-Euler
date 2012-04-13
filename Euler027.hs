8859717fd6f636efcfa6695b458d1c4560193e5e
Shengyi Wang <txyyss@gmail.com>
2012-04-13 20:03:43 +0800

    Solve Problem 27 and 28

---
+++ Euler027.hs
@@ -0,0 +1,75 @@
+-- Euler published the remarkable quadratic formula:
+
+-- n² + n + 41
+
+-- It turns out that the formula will produce 40 primes for the
+-- consecutive values n = 0 to 39. However, when n = 40, 40^2 + 40 +
+-- 41 = 40(40 + 1) + 41 is divisible by 41, and certainly when n = 41,
+-- 41² + 41 + 41 is clearly divisible by 41.
+
+-- Using computers, the incredible formula n² - 79n + 1601 was
+-- discovered, which produces 80 primes for the consecutive values n =
+-- 0 to 79. The product of the coefficients, 79 and 1601, is 126479.
+
+-- Considering quadratics of the form:
+
+-- n² + an + b, where |a| < 1000 and |b| < 1000
+
+-- where |n| is the modulus/absolute value of n
+-- e.g. |11| = 11 and |4| = 4
+
+-- Find the product of the coefficients, a and b, for the quadratic
+-- expression that produces the maximum number of primes for
+-- consecutive values of n, starting with n = 0.
+
+module Euler where
+
+import Data.List (maximumBy)
+import Data.Ord (comparing)
+
+minus :: Ord a => [a] -> [a] -> [a]
+minus lx@(x:xs) ly@(y:ys) = case (compare x y) of 
+  LT -> x : minus xs ly
+  EQ ->     minus xs ys 
+  GT ->     minus lx ys
+minus xs _ = xs
+
+primes = 2 : primes'
+  where
+    primes' = sieve [3,5..] 9 primes'
+    sieve (x:xs) q ps@ ~(p:t)
+      | x < q     = x : sieve xs q ps
+      | otherwise =     sieve (xs `minus` [q, q+2*p..]) (head t^2) t
+
+isPrime :: Int -> Bool
+isPrime n 
+  | n <= 1 = False
+  | otherwise = tryDivide primes
+  where tryDivide (x:xs)
+          | x * x > n = True
+          | n `mod` x == 0 = False
+          | otherwise = tryDivide xs
+                        
+primesTo :: Int -> [Int]
+primesTo n = helper [] primes
+  where helper existed (x:xs)
+          | x > n = reverse existed
+          | otherwise = helper (x:existed) xs
+
+consecutivePrimeLength :: [Int] -> Int
+consecutivePrimeLength xs = helper 0 xs
+  where helper curr [] = curr
+        helper curr (y:ys)
+          | isPrime y = helper (curr + 1) ys
+          | otherwise = curr
+                        
+bCandidates = primesTo 1000
+
+abCandidates = [(a,b) | b <- bCandidates, a <- [((-b - 4) `div` 2 + 1)..1000]]
+
+lenOfPrimes :: (Int, Int) -> Int
+lenOfPrimes x = consecutivePrimeLength $ map (quadratic x) [0..]
+  where quadratic (a, b) n = n * (n + a) + b
+
+result027 = (fst maxUnder1000) * (snd maxUnder1000)
+  where maxUnder1000 = fst $ maximumBy (comparing snd) $ zip abCandidates (map lenOfPrimes abCandidates)
---
+++ Euler028.hs
@@ -0,0 +1,26 @@
+-- Starting with the number 1 and moving to the right in a clockwise
+-- direction a 5 by 5 spiral is formed as follows:
+
+-- 21 22 23 24 25
+-- 20  7  8  9 10
+-- 19  6  1  2 11
+-- 18  5  4  3 12
+-- 17 16 15 14 13
+
+-- It can be verified that the sum of the numbers on the diagonals is
+-- 101.
+
+-- What is the sum of the numbers on the diagonals in a 1001 by 1001
+-- spiral formed in the same way?
+
+module Euler where
+
+sum4 :: Int -> Int
+sum4 n = n * (4 * n - 6) + 6
+
+diagonalsSum :: Int -> Int
+diagonalsSum n = helper 0 n
+  where helper curr 1 = curr + 1
+        helper curr m = helper (curr + sum4 m) (m-2)
+
+result028 = diagonalsSum 1001
