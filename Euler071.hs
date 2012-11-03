634d7be7ec127d877843a3a39f7620d7b06479ca
Shengyi Wang <txyyss@gmail.com>
2012-11-03 22:32:29 +0800

    Solve Problem 71.

---
+++ Euler071.hs
@@ -0,0 +1,30 @@
+-- Consider the fraction, n/d, where n and d are positive integers. If
+-- nd and HCF(n,d)=1, it is called a reduced proper fraction.
+
+-- If we list the set of reduced proper fractions for d <= 8 in
+-- ascending order of size, we get:
+
+-- 1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5,
+-- 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+
+-- It can be seen that 2/5 is the fraction immediately to the left of
+-- 3/7.
+
+-- By listing the set of reduced proper fractions for d <= 1,000,000
+-- in ascending order of size, find the numerator of the fraction
+-- immediately to the left of 3/7.
+
+module Euler071 where
+
+import Data.List
+
+leftNearest :: (Int, Int) -> Int -> Int
+leftNearest (hiN,hiD) d
+  | d == hiD = helper (hiN - 1)
+  | otherwise = helper (d * hiN `div` hiD)
+  where helper 0 = 0
+        helper x
+          | gcd x d == 1 = x
+          | otherwise = helper (x-1)
+
+result071 = snd . maximumBy (\(a,b) (c,d) -> compare (b * c) (a * d)) . zip [3..] $ map (leftNearest (3,7)) [3..1000000]
