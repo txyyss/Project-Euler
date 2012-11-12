59e4b3707e43abc10b8c7e7d0d18d49d8e6f42ee
Shengyi Wang <txyyss@gmail.com>
2012-11-12 19:33:10 +0800

    Delete unuseful code in 81.

--- Euler081.hs
+++ Euler081.hs
@@ -26,18 +26,4 @@ minPathSum (m:ms) = helper (scanl1 (+) m) ms
 getMatrix :: String -> [[Int]]
 getMatrix = map (\x -> read $ "[" ++ x ++ "]") . lines
 
-inf = 10^9
-
-solve last_row [] = last last_row
-solve last_row (r:rs) = solve nr rs
-  where
-    r2 = zip r $ map (min inf) $ zipWith (+) last_row r
-    minv a (b,c) = min (a+b) c
-    nr = tail $ scanl minv inf r2
-
-problem_81 content = solve (0 : repeat inf) matrix
-  where
-    matrix :: [[Int]]
-    matrix = map (\x -> read $ "[" ++ x ++ "]") . lines $ content
-
 result081 = fmap (minPathSum . getMatrix) $ readFile "data/matrix.txt"
